c5b1fa3215ce1bce604e2adf8b5fcaaa435f0746
idaswk <idawatson@hotmail.com>
2023-01-22 15:45:58 +0100

    add changes to form validation

---
+++ javascript/form.js
@@ -0,0 +1,302 @@
+/* --------- */
+/* VARIABLES */
+/* --------- */
+
+const titleInput = document.querySelector("#title");
+const firstNameInput = document.querySelector("#first-name");
+const lastNameInput = document.querySelector("#last-name");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+const addressInput = document.querySelector("#address");
+const postalCodeInput = document.querySelector("#postal-code");
+const areaInput = document.querySelector("#area");
+// projectInput = document.querySelector("#project");
+const messageInput = document.querySelector("#message");
+const formSubmit = document.querySelector("#submit");
+
+const nameRegEx = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{0,}$/;
+let firstName, lastName, email, phone, address, postalCode, area, message;
+
+let data = {};
+let validationErrors = {
+  firstName,
+  lastName,
+  email,
+  phone,
+  address,
+  postalCode,
+  area,
+  message,
+};
+
+/* --------- */
+/* FUNCTIONS */
+/* --------- */
+
+// function titleValidation() {
+//   data.titleData = titleInput.value;
+// }
+
+// function detectErrorMessage() {
+//   if (document.querySelector("form span")) {
+//     document.querySelectorAll("form span").forEach((spanElement) => {
+//       spanElement.remove();
+//     });
+//   }
+// }
+
+function firstNameValidation() {
+  function detectErrorMessage() {
+    if (document.querySelector(".first-name-container span")) {
+      document
+        .querySelectorAll(".first-name-container span")
+        .forEach((spanElement) => {
+          spanElement.remove();
+        });
+    }
+  }
+
+  function displayErrorMessage() {
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerHTML = validationErrors.firstName;
+    document.querySelector(".first-name-container label").after(errorDisplay);
+    firstNameInput.style.borderBottomColor = "rgb(246, 78, 78)";
+  }
+
+  firstName = firstNameInput.value;
+  if (!firstName) {
+    console.error("No first name provided");
+    validationErrors.firstName = "Please provide a first name";
+    detectErrorMessage();
+    displayErrorMessage();
+  } else {
+    if (!nameRegEx.test(firstName)) {
+      console.error("Invalid first name");
+      validationErrors.firstName =
+        "Invalid format - please avoid special characters";
+      detectErrorMessage();
+      displayErrorMessage();
+    } else {
+      console.info(`First name (${firstName}) is valid`);
+      delete validationErrors.firstName;
+      detectErrorMessage();
+      firstNameInput.style.borderBottom = "solid 2px green";
+      console.log(validationErrors);
+    }
+  }
+}
+
+console.log(validationErrors);
+
+function lastNameValidation() {
+  function detectErrorMessage() {
+    if (document.querySelector(".last-name-container span")) {
+      document
+        .querySelectorAll(".last-name-container span")
+        .forEach((spanElement) => {
+          spanElement.remove();
+        });
+    }
+  }
+
+  function displayErrorMessage() {
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerHTML = validationErrors.lastName;
+    document.querySelector(".last-name-container label").after(errorDisplay);
+    lastNameInput.style.borderBottomColor = "rgb(246, 78, 78)";
+  }
+
+  lastName = lastNameInput.value;
+  if (!lastName) {
+    console.error("No last name provided");
+    validationErrors.lastName = "Please provide a last name";
+    detectErrorMessage();
+    displayErrorMessage();
+  } else {
+    if (!nameRegEx.test(lastName)) {
+      console.error("Invalid last name");
+      validationErrors.lastName =
+        "Invalid format - please avoid special characters and numbers";
+      detectErrorMessage();
+      displayErrorMessage();
+    } else {
+      console.info(`Last name (${lastName}) is valid`);
+      delete validationErrors.lastName;
+      detectErrorMessage();
+      lastNameInput.style.borderBottomColor = "green";
+      console.log(validationErrors);
+    }
+  }
+}
+
+function emailValidation() {
+  // detectErrorMessage();
+
+  function displayErrorMessage() {
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerHTML = validationErrors.email;
+    emailInput.after(errorDisplay);
+    emailInput.style.borderBottomColor = "rgb(246, 78, 78)";
+  }
+
+  email = emailInput.value;
+  if (!email) {
+    console.error("Please enter valid E-Mail address");
+    validationErrors.email = "Please provide an E-Mail address";
+    displayErrorMessage();
+  } else {
+    const emailRegEx =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegEx.test(email)) {
+      console.error("Invalid E-Mail");
+      validationErrors.email = "Invalid format (max@mustermann.ch)";
+      displayErrorMessage();
+    } else {
+      console.info("Valid E-Mail address :)");
+      delete validationErrors.email;
+      console.log(validationErrors);
+    }
+  }
+}
+
+function phoneValidation() {
+  // detectErrorMessage();
+
+  function displayErrorMessage() {
+    const errorDisplay = document.createElement("span");
+    errorDisplay.innerHTML = validationErrors.phone;
+    phoneInput.after(errorDisplay);
+    phoneInput.style.borderBottomColor = "rgb(246, 78, 78)";
+  }
+
+  phone = phoneInput.value;
+  if (!phone) {
+    console.error("No phone number provided");
+    validationErrors.phone = "No phone number provided";
+    displayErrorMessage();
+  } else {
+    const phoneRegEx = /^(\+|00)(?:[0-9] ?){6,14}[0-9]$/;
+    if (!phoneRegEx.test(phone)) {
+      console.error("Invalid phone number");
+      validationErrors.phone =
+        "Invalid format - please include 00 or +, followed by the country code";
+      displayErrorMessage();
+    } else {
+      console.info(`Phone number (${phone}) is valid`);
+      delete validationErrors.phone;
+      console.log(validationErrors);
+    }
+  }
+}
+
+function addressValidation() {
+  address = addressInput.value;
+  if (!address) {
+    console.error("No address provided");
+    validationErrors.address = "No address provided";
+  } else {
+    console.info(`Address provided: ${address}`);
+    delete validationErrors.address;
+    console.log(validationErrors);
+  }
+}
+
+function postalCodeValidation() {
+  postalCode = postalCodeInput.value;
+  if (!postalCode) {
+    console.error("No postal code provided");
+    validationErrors.postalCode = "No postal code provided";
+  } else {
+    const zipRegExGeneral = /^[a-z0-9][a-z0-9\- ]{0,10}[a-z0-9]$/;
+    const zipRegExCanada = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+    const zipRegExUK =
+      /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/;
+    if (
+      !(
+        zipRegExGeneral.test(postalCode) ||
+        zipRegExCanada.test(postalCode) ||
+        zipRegExUK.test(postalCode)
+      )
+    ) {
+      console.error("Invalid postal code");
+      validationErrors.postalCode = "Invalid postal code";
+    } else {
+      console.info(`Postal code (${postalCode}) is valid`);
+      delete validationErrors.postalCode;
+      console.log(validationErrors);
+    }
+  }
+}
+
+function areaValidation() {
+  area = areaInput.value;
+  if (!area) {
+    console.error("No location provided");
+    validationErrors.area = "No location provided";
+  } else {
+    console.info(`Location provided: ${area}`);
+    delete validationErrors.area;
+    console.log(validationErrors);
+  }
+}
+
+function messageValidation() {
+  message = messageInput.value;
+  if (!message) {
+    console.error("No message provided");
+    validationErrors.message = "No message provided";
+  } else {
+    if (message.length <= 30) {
+      console.error("Not enough characters");
+      validationErrors.message = "Not enough characters (min. 30)";
+    } else {
+      console.info("Message has enough characters");
+      delete validationErrors.message;
+      console.log(validationErrors);
+    }
+  }
+}
+
+function validateForm(event) {
+  event.preventDefault();
+
+  // data and validationErrors are both objects, because we have defined them as such (data = {})
+  // First (theoretically) we want to add the input data of the user to the
+  // data object we have defined, so we can later send this information to
+  // the backend.
+  data.firstName = firstName;
+  data.lastName = lastName;
+  data.email = email;
+  data.phone = phone;
+  data.address = address;
+  data.postalCode = postalCode;
+  data.area = area;
+  data.message = message;
+
+  // We then check to see if there are still any errors
+  if (Object.keys(validationErrors).length > 0) {
+    // If there are still validation errors, log an error to the console
+    console.error("Form is not complete");
+  } else {
+    // If there are no validation errors,
+    // send form to backend (we use console.log because we have not worked with backend yet)
+    console.log("Sending form data to backend");
+  }
+
+  console.log(data);
+}
+
+/* --------------- */
+/* EVENT LISTENERS */
+/* --------------- */
+
+// titleInput.addEventListener("focusout", titleValidation);
+firstNameInput.addEventListener("focusout", firstNameValidation);
+lastNameInput.addEventListener("focusout", lastNameValidation);
+emailInput.addEventListener("focusout", emailValidation);
+phoneInput.addEventListener("focusout", phoneValidation);
+addressInput.addEventListener("focusout", addressValidation);
+postalCodeInput.addEventListener("focusout", postalCodeValidation);
+areaInput.addEventListener("focusout", areaValidation);
+messageInput.addEventListener("focusout", messageValidation);
+formSubmit.addEventListener("click", validateForm);
--- javascript/gallery.js
+++ javascript/gallery.js
@@ -2,53 +2,78 @@
 /* QUERY SELECTORS */
 /* --------------- */
 
+// Query selectors for the images
 const lightboxEnabled = document.querySelectorAll(".lightbox-enabled");
 const lightboxArray = Array.from(lightboxEnabled);
-const lastImage = lightboxArray.length - 1;
+const prevImage = lightboxArray.length - 1;
 const lightboxContainer = document.querySelector(".lightbox-container");
 const lightboxImage = document.querySelector(".lightbox-image");
 
+// Query selectors for the buttons
 const lightboxBtns = document.querySelectorAll(".lightbox-btn");
 const lightboxBtnRight = document.querySelector("#right");
 const lightboxBtnLeft = document.querySelector("#left");
 
-let activeImage;
+const slideshowBtn = document.querySelector(".slideshow-btn");
+let playing = true;
+// let slideInterval = setInterval(slideLeft, 3000);
+let slideInterval, activeImage;
+
+// let activeImage;
 
 /* --------- */
 /* FUNCTIONS */
 /* --------- */
 
 // Adding the class "active" to lightboxContainer
-showLightBox = function () {
+function pauseSlideshow() {
+  playing = false;
+  clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+  playing = true;
+  slideInterval = setInterval(slideLeft, 3000);
+}
+
+function resetInterval() {
+  clearInterval(slideInterval);
+  playSlideshow();
+}
+
+// Adding the class "active" to lightboxContainer
+openLightbox = function () {
   lightboxContainer.classList.add("active");
+  resetInterval();
 };
 
-// Removing the class "active" to lightboxContainer
-hideLightBox = function () {
+// Removing the class "active" from lightboxContainer
+closeLightbox = function () {
   lightboxContainer.classList.remove("active");
+  playing = true;
 };
 
-setActiveImage = function (image) {
+setCurrentImage = function (image) {
   lightboxImage.src = image.dataset.imagesrc;
   activeImage = lightboxArray.indexOf(image);
 };
 
-const transitionSlidesLeft = () => {
+const slideLeft = () => {
   lightboxBtnLeft.focus();
-  activeImage === lastImage
-    ? setActiveImage(lightboxArray[0])
-    : setActiveImage(lightboxArray[activeImage].nextElementSibling);
+  activeImage === prevImage
+    ? setCurrentImage(lightboxArray[0])
+    : setCurrentImage(lightboxArray[activeImage].nextElementSibling);
 };
 
-const transitionSlidesRight = () => {
+const slideRight = () => {
   lightboxBtnLeft.focus();
   activeImage === 0
-    ? setActiveImage(lightboxArray[lastImage])
-    : setActiveImage(lightboxArray[activeImage].previousElementSibling);
+    ? setCurrentImage(lightboxArray[prevImage])
+    : setCurrentImage(lightboxArray[activeImage].previousElementSibling);
 };
 
 const transitionSlideHandler = (moveItem) => {
-  moveItem.includes("left" ? transitionSlidesLeft() : transitionSlidesRight());
+  moveItem.includes("left" ? slideLeft() : slideRight());
 };
 
 /* --------------- */
@@ -57,19 +82,44 @@ const transitionSlideHandler = (moveItem) => {
 
 lightboxEnabled.forEach((image) => {
   image.addEventListener("click", () => {
-    showLightBox();
-    setActiveImage(image);
+    openLightbox();
+    setCurrentImage(image);
   });
 });
 
 lightboxContainer.addEventListener("click", () => {
-  hideLightBox();
+  closeLightbox();
+});
+
+slideshowBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  // checks to see if the lightboxContainer is active, if not the eventListener
+  // will return/exit
+  if (!lightboxContainer.classList.contains("active")) return;
+  // checks if the spacebar is pressed
+  if (e.key === " ") {
+    e.preventDefault();
+    if (playing) {
+      pauseSlideshow();
+    } else {
+      playSlideshow();
+    }
+  }
 });
 
 lightboxBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.stopPropagation();
     transitionSlideHandler(e.currentTarget.id);
+    resetInterval();
   });
 });
 
@@ -80,10 +130,11 @@ window.addEventListener("keydown", (e) => {
   if (!lightboxContainer.classList.contains("active")) return;
   // checks if the left or right arrows on the keyboard are pressed
   if (e.key === "Escape") {
-    hideLightBox();
+    closeLightbox();
   }
   if (e.key.includes("Left") || e.key.includes("Right")) {
     e.preventDefault();
     transitionSlideHandler(e.key.toLowerCase());
+    resetInterval();
   }
 });
